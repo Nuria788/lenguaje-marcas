347bb6bf90ba16ba9d9690b5637492e3d1c1b0b8
Nuria788 <200093@iessierradeguara.com>
2022-07-03 14:55:03 +0200

    ..

---
+++ JulioCodesSM.html/CircularProgressBar.html/app.js
@@ -0,0 +1,10 @@
+let counter = 0;
+setInterval(() => {
+    const percentageNumber = document.querySelector('.number');
+    const percentageNumberValue = Number(percentageNumber.getAttribute('data-value'));
+    if (counter !== percentageNumberValue) {
+        counter++;
+        percentageNumber.innerHTML = `${counter}%`;
+    }
+}, 80);
+
